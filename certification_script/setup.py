8ce78ed3e406aa0f7fe7e480b683e5618a4a85e3
gstepanov <gstepanov@mirantis.com>
2014-12-03 15:54:03 +0200

    packages versions updated
    Change-Id: I356d2e07572d3311b0cfc6a49d4d9254b15b4d8c

--- certification_script/setup.py
+++ certification_script/setup.py
@@ -9,7 +9,7 @@ setup(
     author='Mirantis Inc.',
     author_email='product@mirantis.com',
     url='http://mirantis.com',
-    install_requires=['PyYAML==3.10', "argparse==1.2.1","requests==2.2.1",
+    install_requires=['PyYAML>=3.10', "argparse>=1.2.1","requests>=2.2.1",
                       'netaddr', 'python-keystoneclient'],
     packages=find_packages(),
     entry_points={

54a50c986ddb2b180ac1ff10868936c78821c08b
koder aka kdanilov <kdanylov@mirantis.com>
2014-12-12 00:27:44 +0200

    update iso build script. add initial state testing, etc

--- certification_script/certification_script/make_iso.sh
+++ certification_script/certification_script/make_iso.sh
@@ -4,11 +4,16 @@ set -e
 set -o pipefail
 
 ISO_PATH=/media/data/fuel-5.1.1-28-2014-11-20_21-01-00.iso
-ISO_MNT_FOLDER=/mnt/iso1
 TMP_FOLDER=/media/fuel_remake_iso
-CERT_PATH=~/workspace/fsert/certification_script/certification_script
-TEST_PATH=~/workspace/fsert/fuelweb_tests
 
+CERT_SCRIPT_GIT_URL=
+TEST_GIT_URL=
+CERT_PATH_ORIGIN=~/workspace/fsert/certification_script/certification_script
+TEST_PATH_ORIGIN=~/workspace/fsert/fuelweb_tests
+
+CERT_PATH="$TMP_FOLDER/cert_script_git"
+TEST_PATH="$TMP_FOLDER/tests_git"
+ISO_MNT_FOLDER="$TMP_FOLDER/old_iso_mount"
 DOCKER_IMAGES_FOLDER="$TMP_FOLDER/container_images"
 DOCKER_IMAGE="$ISO_MNT_FOLDER/docker/images/fuel-images.tar.lrz"
 NEW_DOCKER_IMAGE="$DOCKER_IMAGES_FOLDER/fuel-images.tar.lrz"
@@ -19,11 +24,91 @@ NAILGUN_CONTENT_TMP_FOLDER="$DOCKER_IMAGES_FOLDER/nailgun_fs"
 IMAGE_SCRIPT_FOLDER="$NAILGUN_CONTENT_TMP_FOLDER/usr/lib/python2.6/site-packages"
 WHOLE_ISO_TMP="$TMP_FOLDER/image"
 SCRIPTS_TMP_FOLDER_FOR_DOCKER="$TMP_FOLDER/files"
+RESULT_ISO="$TMP_FOLDER/fuel_cert_iso.iso"
+MIN_FREE_SPACE=13728640
+
+function remote_old_nailgun_container() {
+    set +e 
+    set +o pipefail
+    NAILGUN_IMAGE_ID=`docker images | grep nailgun | awk '{print $3}'`
+    set -e 
+    set -o pipefail
+    
+    if [ ! -z "$NAILGUN_IMAGE_ID" ] ; then
+        docker rmi $NAILGUN_IMAGE_ID
+    fi    
+}
+
+function test_environment() {
+    if [ ! -f "$ISO_PATH" ]; then
+        echo "ERROR: Base iso file not found at $ISO_PATH"
+        exit 1
+    fi
+
+    set +e 
+    set +o pipefail
+
+    which lrunzip > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Can't found lrunzip. Install it with sudo apt-get install lrunzip"
+        exit 1
+    fi
+
+    which docker > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Can't found docker. Install it with sudo apt-get install docker.io, add youself to docker group and relogin"
+        exit 1
+    fi
+
+    which git > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Can't found git. Install it with sudo apt-get install git"
+        exit 1
+    fi
+
+    docker images > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "ERROR: 'docker images' failed"
+        exit 1
+    fi
+
+    which mkisofs > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Can't found mkisofs"
+        exit 1
+    fi
+
+    # check free space
+    TMP_FREE_SPACE=`df -k "$TMP_FOLDER" | tail -n1 | awk '{print $4}'`
+    if [ "$TMP_FREE_SPACE" -lt "$MIN_FREE_SPACE" ] ; then
+        echo "At least 1024 * $MIN_FREE_SPACE free space required, but only $TMP_FREE_SPACE available. Free some space and restart"
+        exit 1
+    fi
+
+    set -e 
+    set -o pipefail
+}
+
+function checkout_code() {
+    set +e 
+    rm -rf "$CERT_PATH"
+    rm -rf "$TEST_PATH"
+    set -e 
+
+    mkdir -p "$CERT_PATH"
+    mkdir -p "$TEST_PATH"
+
+    cp -r "$CERT_PATH_ORIGIN/." "$CERT_PATH"
+    cp -r "$TEST_PATH_ORIGIN/." "$TEST_PATH"
+
+    # git clone "$CERT_SCRIPT_GIT_URL" "$CERT_PATH"
+    # git clone "$TEST_GIT_URL" "$TEST_PATH"
+}
 
 function repack_containers_archive() {
-    pushd $TMP_FOLDER
     rm -f "$WHOLE_TAR_FILE"
-    sudo tar -cf "$WHOLE_TAR_FILE" *
+    pushd $TMP_FOLDER/container_images
+    sudo tar -cf "$WHOLE_TAR_FILE" *.tar
     popd
 
     rm -f "$NEW_DOCKER_IMAGE"
@@ -31,16 +116,6 @@ function repack_containers_archive() {
 }
 
 function using_docker_file() {
-    set +e 
-    set +o pipefail
-    NAILGUN_IMAGE_ID=`docker images | grep nailgun | awk '{print $3}'`
-    set -e 
-    set -o pipefail
-
-    
-    if [ ! -z "$NAILGUN_IMAGE_ID" ] ; then
-        docker rmi $NAILGUN_IMAGE_ID
-    fi
 
     docker load -i "$NAILGUIN_TAR_FILE"
     NAILGUN_IMAGE_NAME=`docker images | grep nailgun | awk '{print $1}'`
@@ -60,9 +135,15 @@ function using_docker_file() {
     docker save $NAILGUN_IMAGE_NAME > $NAILGUIN_TAR_FILE
 }
 
-function clean() {
-    sudo umount "$ISO_MNT_FOLDER"
-    rm -rf $TMP_FOLDER/*
+function remove_dirs() {
+    sudo rm -rf "$NAILGUN_CONTAINER_TMP_FOLDER"
+    sudo rm -rf "$NAILGUN_CONTENT_TMP_FOLDER"
+    sudo rm -rf "$WHOLE_ISO_TMP"
+    sudo rm -rf "$SCRIPTS_TMP_FOLDER_FOR_DOCKER"
+    sudo rm -rf "$DOCKER_IMAGES_FOLDER"
+    sudo rm -rf "$ISO_MNT_FOLDER"
+    sudo rm -rf "$CERT_PATH"
+    sudo rm -rf "$TEST_PATH"
 }
 
 function prepare_dirs() {
@@ -72,6 +153,7 @@ function prepare_dirs() {
     mkdir -p "$WHOLE_ISO_TMP"
     mkdir -p "$SCRIPTS_TMP_FOLDER_FOR_DOCKER"
     mkdir -p "$DOCKER_IMAGES_FOLDER"
+    mkdir -p "$ISO_MNT_FOLDER"
 }
 
 function remake_iso() {
@@ -85,30 +167,35 @@ function remake_iso() {
 
     sudo mkisofs -r -V "FUEL_CERT_ISO" -J -T -R -b isolinux/isolinux.bin \
                 -no-emul-boot -boot-load-size 4 -boot-info-table \
-                -o "$TMP_FOLDER/fuel_cert_iso.iso" "$WHOLE_ISO_TMP"
+                -o "$RESULT_ISO" "$WHOLE_ISO_TMP"
 }
 
 function main() {
+    remote_old_nailgun_container
     prepare_dirs
+    test_environment
+    checkout_code
 
     # mount iso image
     sudo mount -o loop "$ISO_PATH" "$ISO_MNT_FOLDER"
 
     # extract_containers_images
+    # lrzuntar creates temporary file in current folder
+    pushd $TMP_FOLDER
     lrzuntar -O "$DOCKER_IMAGES_FOLDER" "$DOCKER_IMAGE" 
+    popd
 
     using_docker_file
     repack_containers_archive
     remake_iso
 
-    echo "Results are stored in $TMP_FOLDER/fuel_cert_iso.iso"
+    # umount iso image
+    sudo umount "$ISO_MNT_FOLDER"
+
+    remove_dirs
+    echo "Results are stored in $RESULT_ISO"
 }
 
 main
-
-
-
-
-
 
 
